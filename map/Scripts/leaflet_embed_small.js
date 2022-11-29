f0b8c1dad8a69dd2dfbbc86eea9c06ced2501051
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2022-11-29 01:31:38 +0000

    Updated to use leaflet-hash-plus Use 1 character as base layer plus more for overlays in "meta". No processing of layers in permalinks yet (that is WIP)

--- map/Scripts/leaflet_embed_small.js
+++ map/Scripts/leaflet_embed_small.js
@@ -3,21 +3,23 @@ var layersControl;
 var zoomControl;
 var ajaxRequest;
 var current_zoom = 9;
-var some_var = 0;
+var current_layer_url;
+
+var localUrl='//192.168.1.173/hot/{z}/{x}/{y}.png';
+var hetznerUrl='//map.atownsend.org.uk/hot/{z}/{x}/{y}.png';
+var osmUrl='//tile.openstreetmap.org/{z}/{x}/{y}.png';
+var deUrl='//a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
+var os201604Url='//{s}.os.openstreetmap.org/layer/gb_os_sv_2016_04/{z}/{x}/{y}.png';
+var oslocalUrl='//{s}.os.openstreetmap.org/layer/gb_os_om_local_2020_04/{z}/{x}/{y}.png';
+
+var boundaryUrl='//192.168.1.173/hot6/{z}/{x}/{y}.png';
+var gps2Url='//gps-b.tile.openstreetmap.org/lines/{z}/{x}/{y}.png';
+var floodedUrl='//192.168.1.173/hot4/{z}/{x}/{y}.png';
+var LA_ProwUrl='https://osm.cycle.travel/rights_of_way/{z}/{x}/{y}.png';
+var novisUrl='//192.168.1.173/hot5/{z}/{x}/{y}.png';
 
 function initmap()
 {
-    var hetznerUrl='//map.atownsend.org.uk/hot/{z}/{x}/{y}.png';
-    var boundaryUrl='//map.atownsend.org.uk/hot6/{z}/{x}/{y}.png';
-    var osmUrl='//tile.openstreetmap.org/{z}/{x}/{y}.png';
-    var deUrl='//a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
-    var os201604Url='//{s}.os.openstreetmap.org/layer/gb_os_sv_2016_04/{z}/{x}/{y}.png';
-    var oslocalUrl='//{s}.os.openstreetmap.org/layer/gb_os_om_local_2020_04/{z}/{x}/{y}.png';
-    var gps2Url='//gps-b.tile.openstreetmap.org/lines/{z}/{x}/{y}.png';
-    var floodedUrl='//map.atownsend.org.uk/hot4/{z}/{x}/{y}.png';
-    var LA_ProwUrl='https://osm.cycle.travel/rights_of_way/{z}/{x}/{y}.png';
-    var novisUrl='//map.atownsend.org.uk/hot5/{z}/{x}/{y}.png';
-
     var osmAttrib='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>';
     var osAttrib='Map data &copy; <a href="https://www.ordnancesurvey.co.uk/business-government/products/open-map-local">Ordnance Survey</a> under <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">OGL</a>';
     var eaAttrib='current flooding &copy; <a href="https://check-for-flooding.service.gov.uk/find-location">Environment Agency</a> under <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">OGL</a>';
@@ -35,6 +37,7 @@ function initmap()
  * We support maxZoom 19 across the board to allow switching from one layer to 
  * another without just getting a grey background.
  * ------------------------------------------------------------------------------ */
+    var localLayer = new L.TileLayer( localUrl, {minZoom: 0, maxZoom: 25, maxNativeZoom: 24, attribution: osmAttrib });
     var hetznerLayer = new L.TileLayer( hetznerUrl, {minZoom: 0, maxZoom: 25, maxNativeZoom: 24, attribution: osmAttrib });
     var boundaryLayer = new L.TileLayer( boundaryUrl, {minZoom: 0, maxZoom: 25, maxNativeZoom: 24, attribution: osmAttrib });
     var osmLayer = new L.TileLayer( osmUrl, {minZoom: 0, maxZoom: 20, maxNativeZoom: 19, attribution: osmAttrib });
@@ -56,12 +59,13 @@ function initmap()
         zoomControl: false,
 	center: new L.LatLng( 53, -1.5 ),
 	zoom: current_zoom,
-	layers: [ hetznerLayer ]
+	layers: [ localLayer ]
     });
 
     map.attributionControl.setPrefix(false);
 
     var baseMaps = {
+	"Local": localLayer,
 	"Default": hetznerLayer,
 	"OSM": osmLayer,
 	"DE": deLayer,
@@ -89,11 +93,101 @@ function initmap()
 
     map.on( 'locationfound', location_success );
     map.on( 'locationerror', location_fail );
+    
+    map.on('baselayerchange', function (e) {
+	console.log("baselayerchange");
+	console.log(e.layer._url);
+	current_layer_url = e.layer._url;
+
+/* ------------------------------------------------------------------------------
+ * At this point the "number of layers" found by counting map.eachLayer will be 1
+ * or 2, even though there is only ever one base layer (if no overlays), and
+ * that number plus an selected overlayes, if overlays are selected.
+ *
+ * Oddly there does not seem to be a way of iterating through the layers added 
+ * to an L.Control.Layers.
+ * ------------------------------------------------------------------------------ */
+	var total_matched_layers = "";
+        map.eachLayer(function(layer){
+	    var current_matched_layer;
+	    current_matched_layer = match_layers( layer._url );
+
+	    console.log(current_matched_layer);
+
+	    if ((( total_matched_layers  === "L" ) ||
+                 ( total_matched_layers  === "H" ) ||
+                 ( total_matched_layers  === "O" ) ||
+                 ( total_matched_layers  === "D" ) ||
+                 ( total_matched_layers  === "6" ) ||
+                 ( total_matched_layers  === "2" )) &&
+		(( current_matched_layer === "L" ) ||
+                 ( current_matched_layer === "H" ) ||
+                 ( current_matched_layer === "O" ) ||
+                 ( current_matched_layer === "D" ) ||
+                 ( current_matched_layer === "6" ) ||
+                 ( current_matched_layer === "2" )))
+		{
+		    console.log("Base layer already added; ignoring");
+		}
+	    else
+		{
+		    total_matched_layers = total_matched_layers + current_matched_layer;
+		}
+	});
+
+	console.log(total_matched_layers);
+
+	if ( total_matched_layers )
+	    hash.setHashMeta(total_matched_layers, false);
+    });
+    
 
 /* ------------------------------------------------------------------------------
  * Add a permalink to the map URL
  * ------------------------------------------------------------------------------ */
     var hash = new L.Hash(map)
+}
+
+
+function match_layers( passed_layer_url )
+{
+    var current_matched_layer = "";
+
+    if ( passed_layer_url === localUrl )
+	current_matched_layer = "L";
+
+    if ( passed_layer_url === hetznerUrl )
+	current_matched_layer = "H";
+
+    if ( passed_layer_url === osmUrl )
+	current_matched_layer = "O";
+
+    if ( passed_layer_url === deUrl )
+	current_matched_layer = "D";
+
+    if ( passed_layer_url === os201604Url )
+	current_matched_layer = "6";
+
+    if ( passed_layer_url === oslocalUrl )
+	current_matched_layer = "2";
+
+    if ( passed_layer_url === boundaryUrl )
+	current_matched_layer = "B";
+
+    if ( passed_layer_url === gps2Url )
+	current_matched_layer = "G";
+
+    if ( passed_layer_url === floodedUrl )
+	current_matched_layer = "F";
+
+    if ( passed_layer_url === LA_ProwUrl )
+	current_matched_layer = "P";
+
+    if ( passed_layer_url === novisUrl )
+	current_matched_layer = "N";
+
+    console.log(current_matched_layer);
+    return current_matched_layer;
 }
 
 
